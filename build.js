7be1775cbbe195027f34385b9669a4913747479a
Da Kong <kongda9999@gmail.com>
2024-10-31 10:05:19 +0800

    fix: build

--- build.js
+++ build.js
@@ -1,9 +1,13 @@
 const { build } = require('esbuild');
 const copyStaticFiles = require('esbuild-copy-static-files');
-const svgr =require('esbuild-plugin-svgr');
+const svgr = require('esbuild-plugin-svgr');
+const fs = require('fs');
 
 
 async function runBuild() {
+    // delete dist folder
+    fs.rm('./dist', { recursive: true, force: true }, (err) => { });
+
     build({
         entryPoints: ['src/content.js', 'src/popup.js', 'src/sw.js', 'src/root.jsx'],
         loader: { '.js': 'jsx', '.png': 'dataurl' },
