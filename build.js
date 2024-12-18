5a50465bf35d02edd110de730d7531f17a283080
Da Kong <kongda9999@gmail.com>
2024-12-18 09:55:18 +0800

    fix: styled components issue

--- build.js
+++ build.js
@@ -11,6 +11,13 @@ async function runBuild() {
     // delete dist folder
     fs.rm('./public/embeddable/index.html', { recursive: true, force: true }, (err) => { });
 
+    try {
+        // find styled-components issue and fix it
+        let dependencyFile = fs.readFileSync('./node_modules/styled-components/dist/styled-components.browser.esm.js', 'utf8');
+        dependencyFile = dependencyFile.replaceAll('process.env.', 'process.env?.')
+        fs.writeFileSync('./node_modules/styled-components/dist/styled-components.browser.esm.js', dependencyFile);
+    } catch (e) { console.log(e) }
+
     build({
         entryPoints: ['src/content.js', 'src/popup.js', 'src/sw.js', 'src/root.jsx'],
         loader: { '.js': 'jsx', '.png': 'dataurl' },
