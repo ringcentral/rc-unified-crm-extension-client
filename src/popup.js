42d25fa02c9e162b1e5f9ae62d468a1ed7185a76
Da Kong <kongda9999@gmail.com>
2025-01-08 13:32:02 +0800

    fix: edit log

--- src/popup.js
+++ src/popup.js
@@ -1199,7 +1199,7 @@ window.addEventListener('message', async (e) => {
                   requireDetails: false
                 });
                 // Case: if create, but found existing log, then edit
-                if (data.body.triggerType = 'editLog' || data.body.triggerType === 'createLog' && !!fetchedCallLogs && fetchedCallLogs.find(l => l.sessionId == data.body.call.sessionId)) {
+                if (data.body.triggerType == 'editLog' || data.body.triggerType === 'createLog' && !!fetchedCallLogs && fetchedCallLogs.find(l => l.sessionId == data.body.call.sessionId)) {
                   data.body.triggerType = 'editLog';
                   fetchedCallLogs = (await getLog({
                     serverUrl: manifest.serverUrl,
