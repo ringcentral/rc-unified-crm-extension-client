8700ca1ac6501a66937b44f0ff2ac7de09d85fa0
SushilMallRC <sushil.mall@ringcentral.com>
2025-05-20 14:51:03 +0530

    redirect to history page

--- src/core/log.js
+++ src/core/log.js
@@ -79,6 +79,13 @@ async function addLog({
                     type: 'rc-adapter-trigger-call-logger-match',
                     sessionIds: [logInfo.sessionId]
                 }, '*');
+                setTimeout(() => {
+                    document.querySelector("#rc-widget-adapter-frame").contentWindow.postMessage({
+                        type: 'rc-adapter-navigate-to',
+                        path: `/history`
+                    }, '*');
+                }, 100);
+
                 break;
             case 'Message':
                 if(logInfo.type === 'Fax')
@@ -106,6 +113,13 @@ async function addLog({
                     }
                     await chrome.storage.local.set({ [`rc-crm-conversation-log-${logInfo.conversationLogId}`]: { logged: true } });
                 }
+                setTimeout(() => {
+                    document.querySelector("#rc-widget-adapter-frame").contentWindow.postMessage({
+                        type: 'rc-adapter-navigate-to',
+                        path: `/messages`
+                    }, '*');
+                }, 100);
+
                 break;
         }
     }
