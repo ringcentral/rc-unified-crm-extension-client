26b153d40ce0c04739d14852c0ce5073bd4796cd
Da Kong <kongda9999@gmail.com>
2025-09-02 16:50:43 +0800

    add: support for link in release notes

--- src/components/releaseNotesPage.js
+++ src/components/releaseNotesPage.js
@@ -15,28 +15,41 @@ async function getReleaseNotesPageRender({ manifest, platformName, registeredVer
             const platformNotes = releaseNotes[manifest.version][platformName] ?? [];
             const allNotes = globalNotes.concat(platformNotes);
             const allTypes = allNotes.map(n => { return n.type }).filter((value, index, array) => { return array.indexOf(value) === index; });
-            let notesRender = [];
-            let notesUiSchema = [];
+            const notesRender = {};
+            const notesUiSchema = {};
+            let counter = 0;
             for (const t of allTypes) {
+                counter++;
                 const targetNotes = allNotes.filter(n => { return n.type === t });
-                notesRender.push({
+                notesRender[`${t}_${counter}`] = {
                     type: 'string',
                     description: t
-                });
-                notesUiSchema.push({
+                };
+                notesUiSchema[`${t}_${counter}`] = {
                     "ui:field": "typography",
                     "ui:variant": "body2", // "caption1", "caption2", "body1", "body2", "subheading2", "subheading1", "title2", "title1"
-                });
+                };
                 for (const n of targetNotes) {
-                    notesRender.push({
+                    counter++;
+                    notesRender[`${t}_${counter}`] = {
                         type: 'string',
                         description: n.description
-                    })
-                    notesUiSchema.push({
-                        "ui:field": "typography",
-                        "ui:variant": "body1", // "caption1", "caption2", "body1", "body2", "subheading2", "subheading1", "title2", "title1"
-                        "ui:style": { margin: '-15px 0px 0px 20px' }
-                    });
+                    };
+                    if (n.link) {
+                        notesUiSchema[`${t}_${counter}`] = {
+                            "ui:field": "link",
+                            "ui:variant": "body1", // "caption1", "caption2", "body1", "body2", "subheading2", "subheading1", "title2", "title1"
+                            "ui:style": { margin: '-15px 0px 0px 20px' },
+                            "ui:href": n.link
+                        };
+                    }
+                    else {
+                        notesUiSchema[`${t}_${counter}`] = {
+                            "ui:field": "typography",
+                            "ui:variant": "body1", // "caption1", "caption2", "body1", "body2", "subheading2", "subheading1", "title2", "title1"
+                            "ui:style": { margin: '-15px 0px 0px 20px' }
+                        };
+                    }
                 }
             }
             return {
