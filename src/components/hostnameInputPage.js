d3c6dbf96d437f3fa85cf4f6292a34279174b8d3
Da Kong <kongda9999@gmail.com>
2025-09-10 14:16:36 +0800

    wip

--- src/components/hostnameInputPage.js
+++ src/components/hostnameInputPage.js
@@ -33,29 +33,13 @@ function getHostnameInputPageRender({ platform, inputUrl, selection, isUrlValid 
             "ui:variant": "body2"
         }
         for (let i = 0; i < platform.environment.instructions.length; i++) {
-            if (platform.environment.instructions[i].startsWith('[')) {
-                const linkText = platform.environment.instructions[i].split('[')[1].split(']')[0];
-                const linkUrl = platform.environment.instructions[i].split(`[${linkText}](`)[1].split(')')[0];
-                page.schema.properties[`instruction${i + 1}`] = {
-                    type: 'string',
-                    description: linkText
-                }
-                page.uiSchema[`instruction${i + 1}`] = {
-                    "ui:field": "link",
-                    "ui:variant": "body1",
-                    "ui:underline": "always",
-                    "ui:href": linkUrl
-                }
+            page.schema.properties[`instruction${i + 1}`] = {
+                type: 'string',
+                description: platform.environment.instructions[i]
             }
-            else {
-                page.schema.properties[`instruction${i + 1}`] = {
-                    type: 'string',
-                    description: platform.environment.instructions[i]
-                }
-                page.uiSchema[`instruction${i + 1}`] = {
-                    "ui:field": "typography", // or typography to show raw text
-                    "ui:bulletedList": true
-                }
+            page.uiSchema[`instruction${i + 1}`] = {
+                "ui:field": "typography", // or typography to show raw text
+                "ui:bulletedList": true
             }
         }
     }
