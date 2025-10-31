d0ba01473e8d68ed493692030f08bee18b8db7d9
Da Kong <kongda9999@gmail.com>
2025-10-31 09:22:21 +0800

    fix: extension number check

--- src/popup.js
+++ src/popup.js
@@ -460,8 +460,8 @@ window.addEventListener('message', async (e) => {
                 const callAutoPopup = userCore.getCallPopSetting(userSettings).value;
                 if (callAutoPopup) {
                   const isExtensionNumber = data.call.direction === 'Inbound' ?
-                    !!data.call.from.extensionNumber :
-                    !!data.call.to.extensionNumber;
+                    (!!data.call.from.extensionNumber && !data.call.from.phoneNumber) :
+                    (!!data.call.to.extensionNumber && !data.call.to.phoneNumber);
 
                   const allowExtensionNumberLogging = userSettings?.allowExtensionNumberLogging?.value ?? false;
                   if (isExtensionNumber && !allowExtensionNumberLogging) {
@@ -1353,7 +1353,8 @@ window.addEventListener('message', async (e) => {
               // extensions numbers should NOT be logged unless explicitly allowed
               const allowExtensionNumberLogging = userSettings?.allowExtensionNumberLogging?.value ?? false;
               const isExtensionNumber = data.body.call.direction === 'Inbound' ?
-                !!data.body.call.from.extensionNumber :
+                (!!data.body.call.from.extensionNumber && !data.body.call.from.phoneNumber) :
+                (!!data.body.call.to.extensionNumber && !data.body.call.to.phoneNumber);
                 !!data.body.call.to.extensionNumber;
               if (!allowExtensionNumberLogging) {
                 if (isExtensionNumber) {
