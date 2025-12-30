8e4eacbb2718f52457b30cbaee5896f3435c9e47
Da Kong <kongda9999@gmail.com>
2025-12-30 11:00:50 +0800

    fix: warm transfer pop up twice

--- src/eventHandlers/rc-call-end-notify.js
+++
@@ -1,7 +0,0 @@
-async function onEvent({ data }) {
-    if (data.call.isOnHold) {
-        await chrome.storage.local.set({ [`${data.call.partyData.sessionId}-transfer-on-hold`]: true });
-    }
-}
-
-exports.onEvent = onEvent;
