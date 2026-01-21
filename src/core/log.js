b1f28b155e8874a8e2bc40abc9d029f0a3c366b6
SushilMallRC <sushil.mall@ringcentral.com>
2026-01-21 16:07:41 +0530

    Typo

--- src/core/log.js
+++ src/core/log.js
@@ -138,7 +138,7 @@ function openLog({ manifest, platformName, hostname, logId, contactType, contact
     window.open(logPageUrl);
 }
 
-async function updateLog({ serverUrl, logType, sessionId, recordingLink, recordingDownloadLink, subject, note, startTime, duration, aiNote, transcript, result, direction, from, to, isShowNotification,additionalSubmission }) {
+async function updateLog({ serverUrl, logType, sessionId, recordingLink, recordingDownloadLink, subject, note, startTime, duration, aiNote, transcript, result, direction, from, to, isShowNotification }) {
     const { rcUnifiedCrmExtJwt } = await chrome.storage.local.get('rcUnifiedCrmExtJwt');
     const { rcAdditionalSubmission } = await chrome.storage.local.get({ rcAdditionalSubmission: {} });
     if (rcUnifiedCrmExtJwt) {
@@ -157,8 +157,7 @@ async function updateLog({ serverUrl, logType, sessionId, recordingLink, recordi
                     result,
                     direction,
                     from,
-                    to,
-                    additionalSubmission
+                    to
                 }
                 const callLogRes = await axios.patch(`${serverUrl}/callLog?jwtToken=${rcUnifiedCrmExtJwt}`, patchBody);
                 if (isShowNotification) {
