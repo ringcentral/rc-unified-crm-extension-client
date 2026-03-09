ef54e3b5e7e658df658864dbf10a8c5da146aff8
DaKingKong <da.kong@ringcentral.com>
2026-03-09 14:31:20 +0800

    quick fix

--- src/core/user.js
+++ src/core/user.js
@@ -3,7 +3,7 @@ import moment from 'moment';
 import { getRcAccessToken } from '../lib/util';
 import { getManifest } from '../service/manifestService';
 import adminCore from './admin';
-import { getServiceManifest } from '../service/embeddableServices';
+import embeddableServices from '../service/embeddableServices';
 import reportPage from '../components/reportPage/reportPage';
 import calldownPage from '../components/calldownPage';
 import { RcAPI } from '../lib/rcAPI';
@@ -181,7 +181,7 @@ async function refreshUserSettings({ changedSettings, isAvoidForceChange = false
     }
     const notificationLevelSetting = getNotificationLevelSetting(userSettings).value;
     await chrome.storage.local.set({ notificationLevelSetting });
-    const serviceManifest = await getServiceManifest();
+    const serviceManifest = await embeddableServices.getServiceManifest();
     document.querySelector("#rc-widget-adapter-frame").contentWindow.postMessage({
         type: 'rc-adapter-register-third-party-service',
         service: serviceManifest
