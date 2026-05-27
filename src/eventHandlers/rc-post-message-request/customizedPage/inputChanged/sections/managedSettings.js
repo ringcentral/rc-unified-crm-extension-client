c17efd4e704e4f8da9460e7ef837dc2e89b37f25
DaKingKong <da.kong@ringcentral.com>
2026-05-27 10:50:10 +0800

    fix: admin setting plugin filed type

--- src/eventHandlers/rc-post-message-request/customizedPage/inputChanged/sections/managedSettings.js
+++ src/eventHandlers/rc-post-message-request/customizedPage/inputChanged/sections/managedSettings.js
@@ -1,6 +1,7 @@
 import managedSettingsPage from '../../../../../components/admin/managedSettingsPage';
 
 async function onEvent({ data, manifest, platformInfo, platformName, platform }) {
+    window.postMessage({ type: 'rc-log-modal-loading-on' }, '*');
     const managedSettingsPageRender = managedSettingsPage.getManagedSettingsPageRender({ crmManifest: platform });
     document.querySelector("#rc-widget-adapter-frame").contentWindow.postMessage({
         type: 'rc-adapter-register-customized-page',
@@ -10,6 +11,7 @@ async function onEvent({ data, manifest, platformInfo, platformName, platform })
         type: 'rc-adapter-navigate-to',
         path: `/customized/${managedSettingsPageRender.id}`, // page id
     }, '*');
+    window.postMessage({ type: 'rc-log-modal-loading-off' }, '*');
 }
 
 exports.onEvent = onEvent;
