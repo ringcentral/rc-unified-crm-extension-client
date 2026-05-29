9685897084e18f151e2923973f8566c797159060
DaKingKong <da.kong@ringcentral.com>
2026-05-29 10:39:57 +0800

    fix: using redirect uri from Dev Console

--- src/core/auth.js
+++ src/core/auth.js
@@ -90,7 +90,7 @@ async function onUserClickConnectButton({ platform, platformName, manifest }) {
                 authUri = buildOAuthUrl({
                     authorizationUri: platform.auth.oauth.authUrl,
                     clientId: platform.auth.oauth.clientId,
-                    redirectUri: 'https://ringcentral.github.io/ringcentral-embeddable/redirect.html',
+                    redirectUri: platform.auth.oauth.redirectUri ?? 'https://ringcentral.github.io/ringcentral-embeddable/redirect.html',
                     scopes: platform.auth.oauth.scope,
                     customState,
                     platformName: platform.name
