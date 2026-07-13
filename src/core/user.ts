76b651b7547d8891f0ce156fa6c98fc4f0c122fc
Sushil Mall <sushil.mall@ringcentral.com>
2026-07-13 18:39:16 +0530

    typo

--- src/core/user.ts
+++ src/core/user.ts
@@ -327,7 +327,6 @@ function getSMSPopSetting(userSettings) {
     }
 }
 
-
 function getIncomingCallPop(userSettings) {
     return {
         value: userSettings?.openContactPageFromIncomingCall?.value ?? 'disabled',
